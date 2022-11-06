350c4ae4075b27e21730e6053c054b406a7432b4
Андрей Никаноров <andrejnikanorov@MacBook-Pro-Andrej.local>
2022-11-06 16:44:12 +0300

    shoulSend test 3 days added

---
+++ src/test/java/ru/netology/CardDeliveryHWTest.java
@@ -0,0 +1,41 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CardDeliveryHWTest {
+
+    public String searchDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldSend() {
+        //Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999");
+        String planningDate = searchDate(3);
+
+        $("[data-test-id=\"city\"] input.input__control").setValue("Петропавловск-Камчатский");
+        $("[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
+        $("[data-test-id=\"name\"] input.input__control").setValue("Андрей Андрей-Андрей");
+        $("[data-test-id=\"phone\"] input.input__control").setValue("+79301111111");
+        $("[data-test-id=\"agreement\"] span.checkbox__box").click();
+        $("[type=\"button\"].button").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+}
